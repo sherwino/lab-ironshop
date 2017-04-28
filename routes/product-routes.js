172789e01b9420267e92e82fa051a69ef1609ed3
Sherwin Garcia-Obregon <sherwination@gmail.com>
2017-04-27 21:51:26 -0400

    v0.1 Added the Edit Page

--- routes/product-routes.js
+++ routes/product-routes.js
@@ -27,7 +27,7 @@ productRoutes.post('/products/new', (req, res, next) => {
     name: req.body.productName,
     price: req.body.productPrice,
     imageUrl: req.body.productImageUrl,
-    description: req.body.productDescrption
+    description: req.body.productDescription
   });
   theProduct.save((err) => {
     if (err) {
@@ -39,8 +39,77 @@ productRoutes.post('/products/new', (req, res, next) => {
   });
 });
 
-productRoutes.get('/product-details', (req, res, next) => {
+//product-details?id=17878787
+//going to use a variable in the url instead
+// productRoutes.get('/product-details', (req, res, next) => {
+//these are URL parameters instead of QUERY which would be req.query.id
+// send will just display the result in the browser on it's own
+//but we are going to use that we are going to use the render
+//and the findById method is a mongoose method that will let you iterate
+// in the DB and find the products by their id...
+// in this case the id is automatically assigned by mongoDB
+// then this will grab the productID that is gathered from the URL
+// rem the req.query will create a key value pair in the url
 
+productRoutes.get('/products/:id', (req, res, next) => {
+ const productId = req.params.id;
+
+
+ Product.findById(productId, (err, theProduct) => {
+   if (err) {
+     next(err);
+     return;
+   }
+   // 404 if no product was found
+   if (!theProduct) {
+     next();
+     return;
+   }
+   res.render('products/product-details-view.ejs', {
+     product: theProduct
+   });
+ });
 });
+//                  /products/444/edit (example of how the URL is going to look)
+productRoutes.get('/products/:id/edit', (req, res, next) => {
+  const productId = req.params.id;
+  Product.findById(productId, (err, theProduct) => {
+    if (err) {
+      next(err);
+      return;
+    }
+  res.render('products/edit-product-view.ejs', {
+    product: theProduct
+  });
+});
+});
+//                      remeber :id is just a placeholder
+//                      it could be whatever you want
+productRoutes.post('/products/:id/', (req, res, next) => {
+  const productID = req.params.id;
+
+  const productChanges = {
+    name:         req.body.productName,
+    price:        req.body.productPrice,
+    imageUrl:     req.body.productImageUrl,
+    description:  req.body.productDescription
+  };
+//this new method has three arguments
+  Product.findByIdAndUpdate(
+    productID,                  //which document to change
+    productChanges,             //variable of the changes you want to make
+    (err, theProduct) => {      //the callback
+      if (err) {
+        next(err);
+        return;
+      }                        //end of error callback
+                              //this is how you would redired to prodcut details page
+                              // res.redirect(`/products/${productId}`);
+
+    res.redirect('/products');
+    }
+  );
+});
+
 
 module.exports = productRoutes;
